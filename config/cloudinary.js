56f86757ab301851aa25f5d7c40cc450c6f85cd3
sridharrapolu <sridharrapolu26@gmail.com>
2026-04-01 01:17:05 +0530

    moved cloudinary config to env

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,8 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dpchl5xkz",
-  api_key: "574444143675862",
-  api_secret: "EH3LDY8hPOSNVRF1oVq8yAPqMM8",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
 module.exports = cloudinary;
